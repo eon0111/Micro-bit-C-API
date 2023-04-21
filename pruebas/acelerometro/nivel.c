d73a1203e6f35844b85f3ebb33333ee7042a49ce
eon0111 <noe.reinosa@hotmail.com>
2023-04-21 16:16:43 +0200

    Actualizado ejemplo nivel.c

--- pruebas/acelerometro/nivel.c
+++ pruebas/acelerometro/nivel.c
@@ -1,22 +1,7 @@
 #include "../../ubit/ubit.h"
-#include <math.h>
-
-float
-inclinacion_eje_x() {
-    float d = atan((float)acelerometro_lectura_z() /
-                   (float)acelerometro_lectura_x()) * RAD_A_GRAD;
-    return (d < 0) ? -90 - d : 90 - d;
-}
-
-float
-inclinacion_eje_y() {
-    float d = atan((float)acelerometro_lectura_z() /
-                   (float)acelerometro_lectura_y()) * RAD_A_GRAD;
-    return (d < 0) ? -90 - d : 90 - d;
-}
 
 int
-get_p(int d)
+get_p(float d)
 {
     return (-9 < d  && d < 9)    ? 2 :
            (-18 < d && d <= -9)  ? 1 :
@@ -29,19 +14,22 @@ main(int n)
 {
     microbit_inicializa_hardware();
 
-    int d_x, d_y, p_x, p_y;
+    float d_x, d_y;
+    int p_x, p_y;
 
     while (1)
     {
         d_x = inclinacion_eje_x();
         d_y = inclinacion_eje_y();
-        
+
         p_x = get_p(d_x);
         p_y = get_p(d_y);
+
+        printf("d_x=%d\td_y=%d\tp_x=%d\tp_y=%d\n", (int)d_x, (int)d_y, p_x, p_y);
 
         display_limpia();
         display_enciende_LED(p_x, p_y);
 
-        timer_delay(100);
+        timer_delay(500);
     }
 }
