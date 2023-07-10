2775f0cc07cf53908694b196c3586f09588b2b5c
eon0111 <noe.reinosa@hotmail.com>
2023-07-10 10:34:53 +0200

    Añadido script de instalación y actualizada librería en el .deb

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# El script debe ejecutar con privilegios de root
+if [ $(id -u) -ne 0 ]
+then
+	printf "$MSJ_NO_ROOT"
+	exit
+fi
+
+apt install -y gcc-arm-none-eabi
+apt install -y libpng-dev
+dpkg -i ubit-1.0-1-amd64
+
+echo "[*] Instalación completada!!"
